18b238422009a9b5d133d615e3bf0067a5f02105
pemudakoding <pemudakoding@gmail.com>
2020-04-22 18:22:05 +0800

    Shayna FrondEnd

---
+++ src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+/**
+ * VIEWS
+ */
+import Home         from '../views/Home.vue';
+import Product      from '../views/Product.vue';
+import ShoppingCart from '../views/ShoppingCart.vue';
+import Success      from '../views/Success.vue';
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/Product',
+    name: 'Product',
+    component: Product
+  },
+  {
+    path: '/Cart',
+    name: 'Cart',
+    component: ShoppingCart
+  },
+  {
+    path: '/Success',
+    name: 'Success',
+    component: Success
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="home">
+		<HeaderShayna/>
+		<HeroSectionShayna/>
+		<BannerShayna/>
+		<InstaShayna/>
+		<PatnerLogoShayna/>
+		<FooterShayna/>
+	</div>
+</template>
+
+<script>
+
+/**
+ * COMPONENT
+ */
+import HeaderShayna 		from '@/components/HeaderShayna.vue';
+import HeroSectionShayna 	from '@/components/HeroSectionShayna.vue';
+import BannerShayna 		from '@/components/BannerShayna.vue';
+import InstaShayna 			from '@/components/InstaShayna.vue';
+import PatnerLogoShayna		from '@/components/PatnerLogoShayna.vue';
+import FooterShayna 		from '@/components/FooterShayna.vue'
+
+/**
+ * Library
+ */
+
+
+export default {
+	name: 'Home',
+	components: {
+		/**
+		 * COMPONENT
+		 */
+		HeaderShayna,
+		HeroSectionShayna,
+		BannerShayna,
+		InstaShayna,
+		PatnerLogoShayna,
+		FooterShayna
+
+		/**
+		 * LIBRARY
+		 */
+		
+	}
+}
+</script>
