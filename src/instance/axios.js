b4a24aec88a5fa7c2787ae1a89ac8ec31f41c3fd
pemudakoding <pemudakoding@gmail.com>
2020-05-05 13:55:34 +0800

    ignore ssl issues

--- src/instance/axios.js
+++ src/instance/axios.js
@@ -1,12 +1,9 @@
 import axios from 'axios';
-import https from 'https';
+
 
 https.globalAgent.options.rejectUnauthorized = false;
 const instance = axios.create({
     baseURL: process.env.VUE_APP_BASE_URL,
-    httpsAgent: new https.Agent({
-    rejectUnauthorized: false,
-})
 });
 
 
